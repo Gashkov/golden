977fb09f4e232b1277c2249b88564145bec8c4c5
Alex Gashkov <gashkov321@gmail.com>
2018-10-04 11:24:01 +0300

    added anchors

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,17 @@ $(document).ready(function(){
      nextArrow: '<i class="fas fa-angle-right right-arrow"></i>',
   });
 });
+
+
+// anchor jquery below
+
+$(document).ready(function() {
+  $(".anchor-link").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 1000);
+    return false;
+  });
+});
